c1dbffd411b02557e67c5e70754f69ebc6c64d20
yoavgurevich <ygurevich@ymail.com>
2014-06-05 15:04:34 -0400

    first draft of route unit tests

--- tests/unit/routes.js
+++ tests/unit/routes.js
@@ -20,7 +20,7 @@ describe('/api/sync Routes', function () {
 });
 
 describe('/api/sync/:syncId/sources route', function () {
-  it('should return a 403 when request.body is set to undefined', function (done){
+  it('should return a 403 if request.body is undefined', function (done){
     util.authenticatedConnection({done: done}, function(err, result) {
       expect(err).not.to.exist;
 
@@ -33,6 +33,72 @@ describe('/api/sync/:syncId/sources route', function () {
         result.done();
       });
       
+    });
+  });
+  it('should return a 201 when sync successfully retrieves path and srcList', function (done){
+    util.authenticatedConnection({done: done}, function(err, result) {
+      expect(err).not.to.exist;
+
+      request.get({
+        url: util.serverURL + '/api/sync/' + result.connectionID + '/sources',
+        jar: result.jar
+      }, function(err, res, body) {
+        expect(err).not.to.exist;
+        expect(res.json).to.deep.equal(201);
+        result.done();
+      });
+
+    });
+  });
+});
+
+describe('/api/sync/:syncId/checksums', function () {
+  it('should return a 200 status code and the checksums after the sync validates', function (done){
+    util.authenticatedConnection({done: done}, function(err, result) {
+      expect(err).not.to.exist;
+
+      request.get({
+        url: util.serverURL + '/api/sync/' + result.connectionID + '/checksums',
+        jar: result.jar
+      }, function(err, res, body) {
+        expect(err).not.to.exist;
+        expect(res.json).to.deep.equal({ 200, { checksums: 'checksums' } });
+        result.done();
+      });
+    });
+  });
+});
+
+describe('/api/sync/:syncId/diffs', function () {
+  it('should return a 200 status code if the req.body.diffs are valid', function (done){
+    util.authenticatedConnection({done: done}, function(err, result) {
+      expect(err).not.to.exist;
+
+      request.get({
+        url: util.serverURL + '/api/sync/' + result.connectionID + '/diffs',
+        jar: result.jar
+      }, function(err, res, body) {
+        expect(err).not.to.exist;
+        expect(res.json).to.deep.equal(200);
+        result.done();
+      });
+    });
+  });
+});
+
+describe('/healthcheck', function () {
+  it('should return a 200 status code and the version if the healthcheck', function (done){
+    util.authenticatedConnection({done: done}, function(err, result) {
+      expect(err).not.to.exist;
+
+      request.get({
+        url: util.serverURL + '/healthcheck',
+        jar: result.jar
+      }, function(err, res, body) {
+        expect(err).not.to.exist;
+        expect(res.json).to.deep.equal({http: 'okay', version: 'version'});
+        result.done();
+      });
     });
   });
 });
